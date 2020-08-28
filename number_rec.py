9766e79f36431bd61791fa0ba31681b97b1aaa9d
Piotr Strzalka <Piotr.Strzalka@bury.com>
2020-08-28 15:16:44 +0200

    Some first attempts to recognize digits

--- number_rec.py
+++ number_rec.py
@@ -1,9 +1,12 @@
+# https://medium.com/machine-learning-algorithms-from-scratch/digit-recognition-from-0-9-using-deep-neural-network-from-scratch-8e6bcf1dbd3
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pylab as pl
 
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 digits = load_digits()
 
@@ -12,13 +15,25 @@ n_samples = len(digits.images)
 print(f"Number of samples in the data set is: {str(n_samples)}")
 
 X = digits.images.reshape((n_samples,-1))
-print(f"Shape of input matrix x is: {str(x.shape)}")
+print(f"Shape of input matrix x is: {str(X.shape)}")
 
 y = digits.target
 print(f"Shape of target vector y is: {str(y.shape)}")
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.2)
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+X_train=X_train.T
+X_test = X_test.T
+y_train = y_train.reshape(y_train.shape[0],1)
+y_test = y_test.reshape(y_test.shape[0],1)
+y_train = y_train.T
+y_test = y_test.T
+
+Y_train = np.zeros((10,y_train.shape[1]))
 
 
 
